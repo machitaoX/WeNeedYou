9fb4940a3b98f984d13df7d47fd2ef4c4915dc44
machitao <1035937382@qq.com>
2021-11-09 23:23:57 +0800

    feat:基础框架

---
+++ weNeedYou-webapi/src/main/java/com/hunan/normal/WeNeedYouWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.hunan.normal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeNeedYouWebapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeNeedYouWebapiApplication.class, args);
+    }
+
+}
---
+++ weNeedYou-webapi/src/test/java/com/hunan/normal/WeNeedYouWebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunan.normal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeNeedYouWebapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
